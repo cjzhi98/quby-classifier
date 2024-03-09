02edf4341f5b93ff4c1856cae8ad27e7d42c1154
cjzhi98 <cjzhi98@gmail.com>
2024-03-09 16:33:01 +0800

    Add description to Quby Classifier interface

--- app.py
+++ app.py
@@ -11,12 +11,25 @@ def predict_quby(image):
     return {"Quby": probs[0].item(), "Not Quby": 1 - probs[0].item()}
 
 
+desc = """
+    <div style="font-family: Arial; padding: 10px; color: #333;">
+        <h2>Quby Classifier</h2>
+        <p>This application classifies images as either "Quby" or "Not Quby".</p>
+        <p>For more details, please visit the project 
+            <a href="https://github.com/cjzhi98/quby-classifier" target="_blank" >
+                here
+            </a>
+        </p>
+    </div>
+"""
+
+
 iface = gr.Interface(
     fn=predict_quby,
     inputs="image",
     outputs="label",
     examples=[["image/quby.png"], ["image/cat.jpeg"]],
+    description=desc,
 )
-
 
 iface.launch(server_name="0.0.0.0", server_port=8000)
